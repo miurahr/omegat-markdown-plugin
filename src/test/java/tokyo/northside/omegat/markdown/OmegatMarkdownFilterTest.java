e2ffa8024f6f65263487cf730742426eedcbfe08
Hiroshi Miura <miurahr@linux.com>
2016-09-08 13:08:07 +0900

    Add table test
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/tokyo/northside/omegat/markdown/OmegatMarkdownFilterTest.java
+++ src/test/java/tokyo/northside/omegat/markdown/OmegatMarkdownFilterTest.java
@@ -96,4 +96,48 @@ class OmegatMarkdownFilterTest extends TestFilterBase {
         OmegatMarkdownFilter mdf = new OmegatMarkdownFilter();
         translateText(mdf, "/source/case1.md");
     }
+
+    @Test
+    void testProcess_table() throws Exception {
+        OmegatMarkdownFilter mdf = new OmegatMarkdownFilter();
+        List<String> entries = parse(mdf, "/source/table.md");
+        List<String> expected = new ArrayList<>();
+        expected.add("Tables");
+        expected.add("First Header");
+        expected.add("Second Header");
+        expected.add("Content Cell");
+        expected.add("Content Cell");
+        expected.add("Content Cell");
+        expected.add("Content Cell");
+        expected.add("Command");
+        expected.add("Description");
+        expected.add("git status");
+        expected.add("List all new or modified files");
+        expected.add("git diff");
+        expected.add("Show file differences that haven't been staged");
+        expected.add("Inline formatting");
+        expected.add("Command");
+        expected.add("Description");
+        expected.add("`git status`");
+        expected.add("List all *new or modified* files");
+        expected.add("`git diff`");
+        expected.add("Show file differences that **haven't been** staged");
+        expected.add("Alignment");
+        expected.add("Left-aligned");
+        expected.add("Center-aligned");
+        expected.add("Right-aligned");
+        expected.add("git status");
+        expected.add("git status");
+        expected.add("git status");
+        expected.add("git diff");
+        expected.add("git diff");
+        expected.add("git diff");
+        assertEquals(entries, expected);
+    }
+
+    @Test
+    void testProcess_table() throws Exception {
+        OmegatMarkdownFilter mdf = new OmegatMarkdownFilter();
+        translateText(mdf, "/source/table.md");
+    }
 }
